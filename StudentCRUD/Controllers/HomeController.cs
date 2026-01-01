b08ea8b104969b30ca0b49a7f7e6e7e00ff1e947
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2026-01-01 09:14:03 +0500

    Authorise Home and Student Controllers

--- StudentCRUD/Controllers/HomeController.cs
+++ StudentCRUD/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using StudentCRUD.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace StudentCRUD.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
